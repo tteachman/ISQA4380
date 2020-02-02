96047bc97609e93658b385975dcf9545bf418218
instructor <pswaminathan@unomaha.edu>
2020-02-02 17:44:40 -0600

    Again68

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    
 
 def post_new(request):
     form = PostForm()
