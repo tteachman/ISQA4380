6a4b8e7dca3d719de707d8af3bdb1e62e2e15278
instructor <pswaminathan@unomaha.edu>
2020-02-02 16:57:12 -0600

    Again66

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 
 # Create your views here.
 
